346361e416353bb0d44801ae77e40dac8f8683eb
abey bruck <abeybruck@gmail.com>
2021-09-05 10:27:59 -0700

    dbCards.js added

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 import express from "express"
 import mongoose from "mongoose"
+import Cards from "./dbCards.js"
 
 // App Config
-
+const app = express()
+const port = process.env.PORT || 8001
+const connection_url = 'mongodb+srv://admin:rhino94@cluster0.7b7df.mongodb.net/tinderdb?retryWrites=true&w=majority'
 // Middlewares
-
+ 
 // DB config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
 
 // API Endpoints
+app.get('/', (req, res) => res.status(200).send('HELLO CLEVER PROGRAMMERS!!!'))
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body
+
+    Cards.create(dbCard, (err, data) => {
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(200).send(data)
+        }
+    })
+})
 
 // Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`))
